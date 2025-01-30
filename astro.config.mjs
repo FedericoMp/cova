8a3afc417b7785d380b23c51a6d31edeae0b09bb
Fede <fedemartinp@gmail.com>
2025-01-30 17:03:18 +0100

    Updated site pages, components and images. Ready to prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://federicomp.github.io',
-    base: 'cova',
+    site: 'https://covacations.com.ar',
 });
